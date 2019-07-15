de030e9724e037ca8e46f8e0f4039e2bfd7bd366
jianbo <563773219@qq.com>
2019-07-15 15:10:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/animview/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.animview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        anim.setAnimResource(R.array.refresh)
+//        anim.setAnimAssets("refresh")
+//        anim.start()
+
+        start.setOnClickListener {
+            anim.start()
+        }
+
+        stop.setOnClickListener {
+            anim.stop()
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                anim.setProgress(progress / 100f)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+    }
+}
